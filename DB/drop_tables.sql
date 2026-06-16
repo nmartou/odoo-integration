c247d7787e4933bdbecf21d264d196166678c576
nmartou <martounicolas@gmail.com>
2026-06-16 16:05:09 +0200

    add specific file for dropping tables

---
+++ DB/drop_tables.sql
@@ -0,0 +1,14 @@
+-- Drop tables
+
+DROP TABLE IF EXISTS payment;
+DROP TABLE IF EXISTS contract_party;
+DROP TABLE IF EXISTS property_history;
+DROP TABLE IF EXISTS contract;
+DROP TABLE IF EXISTS project_subcontractor;
+DROP TABLE IF EXISTS project;
+DROP TABLE IF EXISTS role_person;
+DROP TABLE IF EXISTS property;
+DROP TABLE IF EXISTS subcontractor;
+DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS bank_account;
+DROP TABLE IF EXISTS address;
