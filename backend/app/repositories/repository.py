848d659577a1cc641821a3f7adec80f0d14d12d4
[Nicolas] <martounicolas@gmail.com>
2026-06-20 18:15:18 +0200

    add repository abstract methods

--- backend/app/repositories/repository.py
+++ backend/app/repositories/repository.py
@@ -1,4 +1,19 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from models.model import IModel
 
 class IRepository(ABC):
-    pass
+    @abstractmethod
+    def get_by_id(self, id: int) -> IModel:
+        pass
+    
+    @abstractmethod
+    def get_all(self) -> list[IModel]:
+        pass
+    
+    @abstractmethod
+    def modify_by_id(self, object: IModel) -> IModel:
+        pass
+    
+    @abstractmethod
+    def add(self, object: IModel) -> IModel:
+        pass
